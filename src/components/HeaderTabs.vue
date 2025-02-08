c6b6cc16fef01b42e939640bed15d9f350bf4e4a
mingyeongwon <wonmingy@gmail.com>
2025-02-09 03:17:13 +0900

    feat: component 생성

---
+++ src/components/HeaderTabs.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- 헤더 영역 -->
+    <div>
+      <h5 class="fw-bold">유어스</h5>
+    </div>
+
+    <!-- 탭 메뉴 영역 -->
+    <div class="d-flex justify-content-between align-items-center my-3">
+      <!-- 왼쪽 탭 메뉴 (전체, 강아지, 고양이) -->
+      <div class="d-flex">
+        <div
+          v-for="(tab, index) in leftTabs"
+          :key="index"
+          class="me-3 cursor-pointer"
+        >
+          {{ tab }}
+        </div>
+      </div>
+      <!-- 오른쪽 탭: 문의하기 -->
+      <div class="cursor-pointer">문의하기</div>
+    </div>
+  </div>
+</template>
+
+<script>
+impo
+</script>
+
+<style scoped></style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,90 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
+  <div>
+    <!-- 헤더 영역 -->
+    <div>
+      <h5 class="fw-bold">유어스</h5>
+    </div>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <!-- 탭 메뉴 영역 -->
+    <div class="d-flex justify-content-between align-items-center">
+      <div class="d-flex">
+        <div class="me-3">전체</div>
+        <div class="me-3">고양이</div>
+        <div>강아지</div>
+      </div>
+      <div>문의하기</div>
+    </div>
+
+    <!-- 카드 영역 -->
+    <!-- 'gy-4' 클래스로 수직 간격을 추가합니다 -->
+    <div class="row mt-4 gy-4">
+      <!-- 카드 1 -->
+      <div class="col-md-3" v-for="allCard in allCards" :key="allCard.title">
+        <div class="card rounded-2">
+          <img
+            :src="allCard.imageSrc"
+            class="card-img-top rounded-2"
+            :alt="allCard.title"
+          />
+          <div class="card-body">
+            <p class="card-text">{{ allCard.title }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<script setup>
+import { ref } from "vue";
+
+const allCards = ref([
+  {
+    imageSrc:
+      "https://yt3.googleusercontent.com/CAHrv1abdyWrYZQpNwwjpeyPd-NcdGHU0jiPV_OO_V2uDG01P-GbfIUQA7bMpkTN08CxRZUu7w=s900-c-k-c0x00ffffff-no-rj",
+    title: "나비야사랑해",
+  },
+  {
+    imageSrc:
+      "https://pbs.twimg.com/profile_images/991667101357129729/PUl9oyq-_400x400.jpg",
+    title: "행동하는 동물사랑",
+  },
+  {
+    imageSrc:
+      "https://yt3.googleusercontent.com/-hFZuaymVFTBXQ6T7bCez93onedLqLrSPmCzE2oUKrNZW5knd-xyEZSlUZ45XfAj7RoPcy11Og=s900-c-k-c0x00ffffff-no-rj",
+    title: "동물행동권 카라",
+  },
+  {
+    imageSrc:
+      "https://yt3.googleusercontent.com/CAHrv1abdyWrYZQpNwwjpeyPd-NcdGHU0jiPV_OO_V2uDG01P-GbfIUQA7bMpkTN08CxRZUu7w=s900-c-k-c0x00ffffff-no-rj",
+    title: "나비야사랑해",
+  },
+  {
+    imageSrc:
+      "https://pbs.twimg.com/profile_images/991667101357129729/PUl9oyq-_400x400.jpg",
+    title: "행동하는 동물사랑",
+  },
+  {
+    imageSrc:
+      "https://yt3.googleusercontent.com/-hFZuaymVFTBXQ6T7bCez93onedLqLrSPmCzE2oUKrNZW5knd-xyEZSlUZ45XfAj7RoPcy11Og=s900-c-k-c0x00ffffff-no-rj",
+    title: "동물행동권 카라",
+  },
+  {
+    imageSrc:
+      "https://yt3.googleusercontent.com/CAHrv1abdyWrYZQpNwwjpeyPd-NcdGHU0jiPV_OO_V2uDG01P-GbfIUQA7bMpkTN08CxRZUu7w=s900-c-k-c0x00ffffff-no-rj",
+    title: "나비야사랑해",
+  },
+  {
+    imageSrc:
+      "https://pbs.twimg.com/profile_images/991667101357129729/PUl9oyq-_400x400.jpg",
+    title: "행동하는 동물사랑",
+  },
+  {
+    imageSrc:
+      "https://yt3.googleusercontent.com/-hFZuaymVFTBXQ6T7bCez93onedLqLrSPmCzE2oUKrNZW5knd-xyEZSlUZ45XfAj7RoPcy11Og=s900-c-k-c0x00ffffff-no-rj",
+    title: "동물행동권 카라",
+  },
+]);
+</script>
+
+<style scoped></style>
